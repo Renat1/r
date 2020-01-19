9eb62e2843493cd3073594e58ac7dbe15c88040f
renat <rhrh-2011@mail.ru>
2020-01-19 13:13:55 +0300

    добавил iostram и переменную a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include <iostream>
 // second commit
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    int a = 5;
+	a++;
     return a.exec();
 }
